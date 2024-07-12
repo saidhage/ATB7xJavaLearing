fb1ca5ebfe8b280598351302b9c9f7e3e4ff5aeb
saidhage <psdhage6@gmail.com>
2024-07-12 06:49:05 +0530

    all files done

---
+++ src/ex_01112024/Lab007.java
@@ -0,0 +1,8 @@
+package ex_01112024;
+
+public class Lab007 {
+    public static void main(String args []) {
+
+        System.out.println("Hello check main method argument no matter of square bracket given to args");
+    }
+}
