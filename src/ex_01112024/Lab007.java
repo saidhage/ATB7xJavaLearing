9b9e1fec8fd77794e4844b57eb8ae3a2d1aa5f54
saidhage <psdhage6@gmail.com>
2024-07-12 06:50:55 +0530

    New files added

--- src/ex_01112024/Lab007.java
+++ src/ex_01112024/Lab007.java
@@ -3,6 +3,7 @@ package ex_01112024;
 public class Lab007 {
     public static void main(String args []) {
 
-        System.out.println("Hello check main method argument no matter of square bracket given to args");
+        System.out.println("check main method argument no matter of square bracket given to args");
+        System.out.println("Sai");
     }
 }
