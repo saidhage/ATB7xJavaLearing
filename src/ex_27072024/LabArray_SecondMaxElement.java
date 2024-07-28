7ff42446475c42a4d0e1428816f9be8e544815d1
saidhage <psdhage6@gmail.com>
2024-07-28 19:54:08 +0530

    New files edited array Pgms

--- src/ex_27072024/LabArray_SecondMaxElement.java
+++ src/ex_27072024/LabArray_SecondMaxElement.java
@@ -4,26 +4,27 @@ public class LabArray_SecondMaxElement {
 
     public static void main (String[] args) {
 
+        int[] salaries = {7, 10, 31, 25, 18, 9, 11};
 
- int[] sal = {7, 10, 4, 13, 35, 28, 9, 8, 17};
+        // Assuming highest and second highest
+        int highest = salaries[0];
+        int second_highest = salaries[0];
+        int third_highest  = salaries[0];
 
-        int highest = sal[0];
-        int second_highest = sal[0];
-
-        for (int i = 0; i < sal.length; i++) {
-
-            if(sal[i]>highest){
-                highest = sal[i];
-            }
-            else if(sal[i] > second_highest){
-                second_highest = sal[i];
+        for (int i = 0; i < salaries.length; i++) {
+            if (salaries[i] > highest) {
+                highest = salaries[i];
+            } else if (salaries[i] > second_highest) {
+                second_highest = salaries[i];
+            } else if (salaries[i] >third_highest) {
+                third_highest = salaries[i];
             }
 
         }
-        System.out.println("MAX Salary : " +highest);
-        System.out.println("Second Salary : " +second_highest);
-
-
+        System.out.println("Highest salary: " + highest);
+        System.out.println("Second Highest salary: " + second_highest);
+        System.out.println("Third Highest salary: " + third_highest);
+        }
 
     }
-}
+
